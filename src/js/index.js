765c6eaf8d1511d40a564cb9ba7453a817b05610
Hong Baojin <13809735212@163.com>
2019-09-19 18:01:23 +0800

    base finish

--- src/js/index.js
+++ src/js/index.js
@@ -1,9 +1,29 @@
 $(function () {
-  $('.sign-in select').bind('change', function () {
-    alert($(this).val());
-  });
+  function fixedHandle(aimEle) {
+    var aimTop = aimEle.offset().top; // 元素距离顶部的高度
+    var aimH = aimEle.outerHeight(); // 元素的高度(包括 padding)
+    var createEle = $("<div></div>");
+    $(createEle).css({
+      height: aimH,
+      marginBottom: '0.4rem',
+      display: 'none'
+    });
+    $(aimEle.parent()).prepend(createEle); // 追加到父元素中，成为最后一个孩子
+    $(window).scroll(function () {
+      if($(window).scrollTop() >= aimTop){
+        aimEle.css({
+          position: 'fixed',
+          top: 0
+        });
+        $(createEle).show();
+      }else{
+        aimEle.css({
+          position: 'static'
+        });
+        $(createEle).hide();
+      }
+    });
+  }
 
-  // $(apply_type).bind('change', function () {
-  //   alert($(this).val());
-  // });
+  fixedHandle($('.bd-zh-ls'));
 });
